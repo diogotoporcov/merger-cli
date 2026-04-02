a962d65153461a377e4ebc8620ba92f815da256f
Diogo Toporcov <diogotoporcov@gmail.com>
2026-04-02 01:27:07 -0300

    chore: remove unused warning suppression logic in `merger_wrapper.py`

--- packaging/merger_wrapper.py
+++ packaging/merger_wrapper.py
@@ -1,16 +1,3 @@
-import warnings
-import sys
-
-# Suppress experimental warnings from pydantic and others
-try:
-    from pydantic import PydanticExperimentalWarning
-    warnings.filterwarnings("ignore", category=PydanticExperimentalWarning)
-except ImportError:
-    pass
-
-# Suppress distutils warning (setuptools replacing distutils)
-warnings.filterwarnings("ignore", "Setuptools is replacing distutils", category=UserWarning)
-
 from merger_cli.cli.main import main
 
 if __name__ == "__main__":
